c44848ef192c97912f10e8037be36bb46f97913e
omri zaher <omriza5@gmail.com>
2021-11-16 22:58:28 +0200

    add: users routes

--- routes/users.route.js
+++ routes/users.route.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const crawl = require("../app/puppeteer");
+const { UserModel } = require("../models/model");
 
 router.get("/", async (req, res) => {
-  crawl();
-  res.send("Hello World");
+  await crawl();
+  const users = await UserModel.find();
+  res.send(users);
 });
 module.exports = router;
