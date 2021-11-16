9d71c1ce51961f92c493217cb2e3988722c0cbd8
timor23 <timors23@protonmail.com>
2021-11-16 16:24:24 +0200

    get htmlData

--- index.js
+++ index.js
@@ -8,33 +8,36 @@ const main = async () => {
     });
     const page = await browser.newPage();
     await page.goto("https://www.linkedin.com/login?fromSignIn=true&trk=nav_header_signin");
-    await page.type("#username", "sahrgh1993@gmail.com");
-    await page.type("#password", "Omre@sahr2");
-    await page.click(".btn__primary--large");
+    await page.type("#username", "timors23@protonmail.com");
+    await page.type("#password", "VZaKnDtM49RV");
+
+    await Promise.all([
+        await page.click(".btn__primary--large"),
+        page.waitForNavigation({waitUntil: 'networkidle2'})
+    ]);
     // console.log(res);
-    // setTimeout(async () => {
-        await page.goto("https://www.linkedin.com/search/results/people/?company=stealth&geoUrn=%5B%22101620260%22%5D&keywords=ceo%20stealth%20OR%20cto%20stealth%20OR%20co-founder%20stealth&origin=GLOBAL_SEARCH_HEADER&sid=RRG&title=coe%20OR%20cto")
-    // }, 5000);
+
+    await page.goto("https://www.linkedin.com/search/results/people/?company=stealth&geoUrn=%5B%22101620260%22%5D&keywords=ceo%20stealth%20OR%20cto%20stealth%20OR%20co-founder%20stealth&origin=GLOBAL_SEARCH_HEADER&sid=RRG&title=coe%20OR%20cto")
 
 
     const htmlContent = await page.content();
     console.log(htmlContent);
-    // const $ =  cheerio.load(htmlContent);
-    // const list = $(".reusable-search__result-container").map((index,element) =>  {
-    //     $(element);
-    //     console.log(element);
-    // }).get();
+    const $ = cheerio.load(htmlContent);
+    const list = $(".reusable-search__result-container").map((index, element) => {
+        $(element);
+        // console.log(element);
+    }).get();
 
     //await page.goto("https://www.linkedin.com/in/timor-salah-aldeen-a831636a/");
 
     // .reusable-search__entity-result-list.list-style-none
-   
+
     // app-aware-link
     // const names = await page.evaluate(()=>  {
-        // const personName = document.querySelectorAll(".reusable-search__result-container")
-        // console.log(5);
-        // let arr = [];
-        // personName.forEach()
+    // const personName = document.querySelectorAll(".reusable-search__result-container")
+    // console.log(5);
+    // let arr = [];
+    // personName.forEach()
     // })
 
 }
