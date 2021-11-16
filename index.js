8507fabfc098ebb14bca000c0a201efea75e029f
sahrghaneem <89399419+sahrghaneem@users.noreply.github.com>
2021-11-16 15:43:31 +0200

    linkedin

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer');
+const cheerio = require('cheerio');
 
 const main = async () => {
     const browser = await puppeteer.launch({
@@ -7,13 +8,34 @@ const main = async () => {
     });
     const page = await browser.newPage();
     await page.goto("https://www.linkedin.com/login?fromSignIn=true&trk=nav_header_signin");
-    await page.type("#username", "miishmohim@gmail.com");
-    await page.type("#password", "123456AP");
-    await page.click(".btn__primary--large.from__button--floating");
-    setTimeout(async () => {
-        await page.goto("https://www.linkedin.com/search/results/people/?network=%5B%22F%22%5D&origin=MEMBER_PROFILE_CANNED_SEARCH&sid=g3s")
-    }, 5000);
+    await page.type("#username", "sahrgh1993@gmail.com");
+    await page.type("#password", "Omre@sahr2");
+    await page.click(".btn__primary--large");
+    // console.log(res);
+    // setTimeout(async () => {
+        await page.goto("https://www.linkedin.com/search/results/people/?company=stealth&geoUrn=%5B%22101620260%22%5D&keywords=ceo%20stealth%20OR%20cto%20stealth%20OR%20co-founder%20stealth&origin=GLOBAL_SEARCH_HEADER&sid=RRG&title=coe%20OR%20cto")
+    // }, 5000);
+
+
+    const htmlContent = await page.content();
+    console.log(htmlContent);
+    // const $ =  cheerio.load(htmlContent);
+    // const list = $(".reusable-search__result-container").map((index,element) =>  {
+    //     $(element);
+    //     console.log(element);
+    // }).get();
+
     //await page.goto("https://www.linkedin.com/in/timor-salah-aldeen-a831636a/");
+
+    // .reusable-search__entity-result-list.list-style-none
+   
+    // app-aware-link
+    // const names = await page.evaluate(()=>  {
+        // const personName = document.querySelectorAll(".reusable-search__result-container")
+        // console.log(5);
+        // let arr = [];
+        // personName.forEach()
+    // })
 
 }
 
